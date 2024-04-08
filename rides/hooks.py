97b7d08c8c5b7e14283476d927c8fa2d5df05d6d
gayatri123y <gayatriyeole22@navgurukul.org>
2024-04-08 17:43:59 +0530

    this is ride management first commit

--- rides/hooks.py
+++ rides/hooks.py
@@ -42,7 +42,7 @@ app_license = "mit"
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+#home_page = "login"
 
 # website user home page (by Role)
 # role_home_page = {
@@ -129,6 +129,12 @@ app_license = "mit"
 # 		"on_trash": "method"
 # 	}
 # }
+
+doc_events={
+    "User":{
+        "validate":"rides.user.validate_last_name",
+    }
+}
 
 # Scheduled Tasks
 # ---------------
@@ -227,3 +233,4 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures=["Notification"]
